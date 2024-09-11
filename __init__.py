289c4ac2474ba09255a8fc1fd95e2fdc690ea2b8
pibakombo <pl.ibakombo@gmail.com>
2024-09-11 10:00:54 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,15 +4,12 @@ from flask import json
 from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
-                                                                                                                                       
+
 app = Flask(__name__)
+
 @app.route("/contact/")
 def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>
-  
-@app.route('/')
-def hello_world():
-    return render_template('hello.html') #Comm
-  
+  return "<h2>Ma page de contact</h2>
+
 if __name__ == "__main__":
   app.run(debug=True)
