9e1e8b6930da2d602db2168029bb9f82f0d1a917
pibakombo <pl.ibakombo@gmail.com>
2024-09-11 10:47:16 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -27,6 +27,10 @@ def meteo():
 def mongraphique():
     return render_template("graphique.html")
 
+@app.route("/histogramme/")
+def mongraphique():
+    return render_template("histogramme.html")
+
 @app.route('/')
 def hello_world():
     return render_template('hello.html')
