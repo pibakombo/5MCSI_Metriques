4c351c95846a40891795f836c60ceb2d8511d373
pibakombo <pl.ibakombo@gmail.com>
2024-09-11 11:36:14 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -24,15 +24,17 @@ def meteo():
     return jsonify(results=results)
 
 @app.route('/commits/')
-def commits():
-    response = urlopen('https://api.github.com/repos/pibakombo/5MCSI_Metriques/commits')
-    raw_content = response.read()
-    json_content = json.loads(raw_content.decode('utf-8'))
-    results = []
-    for list_element in json_content.get('list', []):
-        date = list_element.get('commit', {}).get('author', {}).get('date')
-        results.append({'Date': date})
-    return jsonify(results=results)
+def index():
+    return render_template('commits.html')
+
+@app.route('/extract-minutes/<date_string>')
+def extract_minutes(date_string):
+    try:
+        date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
+        minutes = date_object.strftime('%Y-%m-%d %H:%M')  # Format minute
+        return jsonify({'minutes': minutes})
+    except ValueError:
+        return jsonify({'error': 'Invalid date format. Use the following format: YYYY-MM-DDTHH:MM:SSZ'}), 400
 
 @app.route("/rapport/")
 def mongraphique():
